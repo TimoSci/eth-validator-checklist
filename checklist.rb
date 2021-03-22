97143143d8e9549ba1bc7e42a2691bf5be7c63ce
TimoSci <me@timosci.net>
2021-03-22 16:03:11 +0100

    add methods that checks whether user exists

--- checklist.rb
+++ checklist.rb
@@ -1,21 +1,13 @@
-class Eth2Checklist
-
-  def initialize(config)
-    @config = config
-  end
-  attr_reader :config
-
-end
-
-
 module Firewall
 
-    def status
+  def status
+    (%x|sudo ufw status|).scan(/^Status:\s*(active)/)[0][0]
+  end
 
-    end
+  def defaults
+  end
 
 end
-
 
 module Clients
 
@@ -25,5 +17,28 @@ module Clients
     query = %x|sudo sytemctl status #{job.to_s}|
     puts query
   end
+
+end
+
+module Users
+
+  def id(user)
+    (%x|id -u #{user.to_s}|).scan(/^(\d)+/)
+  end
+
+end
+
+
+
+class Eth2Checklist
+
+  def initialize(config)
+    @config = config
+  end
+  attr_reader :config
+
+  include Firewall
+  include Clients
+  include Users
 
 end
