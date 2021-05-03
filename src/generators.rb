bf3486818850bac7359fd6269cfb92e67d48c3d4
TimoSci <me@timosci.net>
2021-05-03 20:57:21 +0200

    loading erb object from template file

--- src/generators.rb
+++ src/generators.rb
@@ -1,4 +1,6 @@
 require 'erb'
+require 'pathname'
+require 'pry'
 
 require_relative 'abstract_classes'
 require_relative 'file_parsing'
@@ -10,14 +12,21 @@ require_relative 'file_parsing'
 
 class ServiceGenerator < Eth2Object
 
-  @@templates_path = '../templates'
+  @@templates_path = 'templates'
 
   include FileParsing::Systemctl
 
   attr_accessor :templates
 
+  def template_path(name)
+    "#{root_path.to_s}/#{templates_path}/#{name}.service.erb"
+  end
+
   def get_template(name)
-    ERB.new File.read('#{templates_path}/#{name}.service.erb')
+    path = template_path(name)
+    binding.pry
+    raise "template file doesn't exist" unless File.exists? path
+    ERB.new File.read(path)
   end
 
   def get_templates
@@ -26,6 +35,20 @@ class ServiceGenerator < Eth2Object
 
   def set_templates
     self.templates = get_templates
+  end
+
+  private
+
+  def root_path
+    @root_path_ || (@root_path = root_path_)
+  end
+
+  def root_path_
+    Pathname.new `pwd`.chomp
+  end
+
+  def templates_path
+    @@templates_path
   end
 
 end
