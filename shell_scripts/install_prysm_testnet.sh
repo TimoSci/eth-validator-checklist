f1bb489141556740d6d994847e6ff7941e2a70ac
TimoSci <me@timosci.net>
2021-05-19 14:18:22 +0200

    add shell script for prysm installation

---
+++ shell_scripts/install_prysm_testnet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone -b v1.3.9 --single-branch https://github.com/prysmaticlabs/prysm.git
+cd prysm
+#
+bazel build //beacon-chain:beacon-chain --config=release
+bazel build //validator:validator --coinfig=release
