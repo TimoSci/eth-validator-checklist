f35d508439c7d688f4a0cf7c99306551d0da3b76
TimoSci <me@timosci.net>
2021-05-25 16:03:31 +0200

    add shell script for installing prysm beacon and validator binaries

--- shell_scripts/install_prysm_mainnet.sh
+++ shell_scripts/install_prysm_mainnet.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-git clone -b v1.3.8-hotfix+6c0942 --single-branch https://github.com/prysmaticlabs/prysm.git
-cd prysm
-#
-bazel build //beacon-chain:beacon-chain --config=release
-bazel build //validator:validator --config=release
+curl -LO https://github.com/prysmaticlabs/prysm/releases/download/v1.3.9/beacon-chain-v1.3.9-linux-amd64
+curl -LO https://github.com/prysmaticlabs/prysm/releases/download/v1.3.9/validator-v1.3.9-linux-amd64
+
+
+mv beacon-chain-v1.0.0-linux-amd64 beacon-chain
+mv validator-v1.0.0-linux-amd64 validator
+
+chmod +x beacon-chain
+chmod +x validator
+
+sudo cp beacon-chain /usr/local/bin
+sudo cp validator /usr/local/bin
