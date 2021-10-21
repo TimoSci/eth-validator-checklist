eff95862d6271476ba3041faf32c9c8211562c4f
Timo <me@timosci.net>
2021-10-21 13:48:57 +0200

    cleanup

--- src/installers.rb
+++ src/installers.rb
@@ -13,10 +13,13 @@ end
 class GethInstaller < Installer
 
     def user
-        config[:users][:prysmbeacon]
+        config[:users][:geth]
     end
 
 end
+
+
+class PrysmIntaller < Installer
 
     # Generates files and services for client
 
@@ -31,24 +34,24 @@ end
         %x|sudo chmod 700 #{datadir}|
     end
 
-    def install_prysm
+    # def install_prysm
     
-        config_beacon = config[:sources][:prysmbeacon]
-        config_validator = config[:sources][:prysmvalidator]
-        %x| curl -LO #{config_beacon[:url]}#{config_beacon[:file]} | 
-        %x| curl -LO #{config_validator[:url]}#{config_validator[:file]} | 
+    #     config_beacon = config[:sources][:prysmbeacon]
+    #     config_validator = config[:sources][:prysmvalidator]
+    #     %x| curl -LO #{config_beacon[:url]}#{config_beacon[:file]} | 
+    #     %x| curl -LO #{config_validator[:url]}#{config_validator[:file]} | 
 
-        %x| mv ./#{config_beacon[:file]} beacon-chain |
-        %x| mv ./#{config_validator[:file]} validator |
+    #     %x| mv ./#{config_beacon[:file]} beacon-chain |
+    #     %x| mv ./#{config_validator[:file]} validator |
 
-        %x| chmod +x beacon-chain |
-        %x| chmod +x validator |
+    #     %x| chmod +x beacon-chain |
+    #     %x| chmod +x validator |
 
-        %x| sudo cp beacon-chain /usr/local/bin |
-        %x| sudo cp validator /usr/local/bin |
-    end
+    #     %x| sudo cp beacon-chain /usr/local/bin |
+    #     %x| sudo cp validator /usr/local/bin |
+    # end
 
-    def install(source= :prysmbeacon, executable_name)
+    def install(source, executable_name)
 
         config_source = config[:sources][source]
         %x| curl -LO #{config_source[:url]}#{config_source[:file]} | 
