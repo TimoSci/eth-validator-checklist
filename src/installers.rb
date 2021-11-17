fd951b802f39f8ea313f4e530ec1acf78056c07d
Timo <me@timosci.net>
2021-11-17 01:37:17 +0100

    add method for deleting user and cleanup

--- src/installers.rb
+++ src/installers.rb
@@ -38,6 +38,10 @@ class PrysmInstaller < Installer
        %| sudo useradd --no-create-home --shell /bin/false #{user} |
     end
 
+    def remove_user
+        %| sudo deluser #{user}|
+    end
+
     def create_data_directory(datadir)
         %x|sudo mkdir -p #{datadir}|
         %x|sudo chown -R #{user}:#{user} #{datadir}|
@@ -48,22 +52,6 @@ class PrysmInstaller < Installer
         %x|sudo trash #{datadir}|
     end
 
-    # def install_prysm
-    
-    #     config_beacon = config[:sources][:prysmbeacon]
-    #     config_validator = config[:sources][:prysmvalidator]
-    #     %x| curl -LO #{config_beacon[:url]}#{config_beacon[:file]} | 
-    #     %x| curl -LO #{config_validator[:url]}#{config_validator[:file]} | 
-
-    #     %x| mv ./#{config_beacon[:file]} beacon-chain |
-    #     %x| mv ./#{config_validator[:file]} validator |
-
-    #     %x| chmod +x beacon-chain |
-    #     %x| chmod +x validator |
-
-    #     %x| sudo cp beacon-chain /usr/local/bin |
-    #     %x| sudo cp validator /usr/local/bin |
-    # end
 
     def latest_version
        checklist.clients.prysmbeacon.latest_version
