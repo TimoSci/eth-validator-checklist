f62e97fba313ab8b6908d248d04d595f7f923026
Timo <me@timosci.net>
2021-11-17 02:06:20 +0100

    create shell script for installing and deploying prysmbeacon

--- shell_scripts/setup.sh
+++ shell_scripts/setup.sh
@@ -4,18 +4,22 @@
 #install gunpg2 which may be required for Ruby
 #
 sudo apt-get install gnupg2
-#ufw
+#
+# Configure ufw
 sudo apt-get install ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 ./ufw_rules.sh
 sudo ufw enable
 #
+# Install and deploy geth
 ./install_geth.sh
 ./setup_geth.sh
 rake generate:service:geth
 ./deploy_geth.sh
 sudo systemctl enable geth
 #
-./setup_beacon.sh
-
+# Install and deploy beacon
+rake install:prysm:beacon
+rake generate:service:prysmbeacon
+./deploy_beacon.sh
