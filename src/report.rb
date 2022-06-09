820fb7df125ffda860d0ce3e67cf6ad40cfc5691
Timo <me@timosci.net>
2022-06-10 01:08:39 +0200

    wrap methods to handle validator data in a separate class to facilitate future format changes

--- src/report.rb
+++ src/report.rb
@@ -9,16 +9,70 @@ class Report
         @clients = clients
         @metrics = metrics
         @default_filename = "validator-balances"
+        @validator_data = ValidatorData.new
+        @validator_data.default_filename = @default_filename
     end
 
-    attr_accessor :clients, :metrics, :default_filename
+    attr_accessor :clients, :metrics, :validator_data
+
+    # def current_validator_balances(epoch)
+    #     pubkeys = metrics.validator_balances_base64.map{|h| h[:pubkey64]}
+    #     clients.prysmbeacon.interface.balances_for_epoch(epoch,pubkeys)
+    # end
 
     def current_validator_balances(epoch)
+        validator_data.balances(epoch)
+    end
+
+    # def write_json(data,file="#{default_filename}.json")
+    #     File.open(file,"w") do |f|
+    #         f.write(data.to_json)
+    #     end
+    # end
+
+    # def read_json(file="#{default_filename}")
+    #     file = "#{file}.json"
+    #     JSON.parse(File.read(file))
+    # end
+
+    # def write_csv(data,file="#{default_filename}.csv")
+    #     CSV.open(file,"wb") do |f|
+    #       data.each do |name,properties|
+    #         f << [name,properties]
+    #       end
+    #     end
+    # end
+
+    def write_current_validator_balances(epoch)
+        validator_data.write_json(current_validator_balances(epoch))
+    end
+
+end
+
+
+class ValidatorData
+
+    attr_accessor :data, :default_filename
+
+    def balances(epoch)
         pubkeys = metrics.validator_balances_base64.map{|h| h[:pubkey64]}
         clients.prysmbeacon.interface.balances_for_epoch(epoch,pubkeys)
     end
 
-    def write_json(data,file="#{default_filename}.json")
+    def get_balances(epoch)
+        self.data = balances(epoch)
+    end
+
+    def to_table(data)
+        titles = ["epoch","public_key","index","balance","status"]
+        out = data.map do |h|
+            balances = h["balances"]
+            [h["epoch"]]+balances.values
+        end
+        titles+out
+    end
+
+    def write_json(file="#{default_filename}.json")
         File.open(file,"w") do |f|
             f.write(data.to_json)
         end
@@ -30,15 +84,11 @@ class Report
     end
 
     def write_csv(data,file="#{default_filename}.csv")
-        CSV.open("#{file}.csv","wb") do |f|
-          data.each do |name,properties|
-            f << [name,properties]
+        CSV.open(file,"wb") do |f|
+          to_table(data).each do |line|
+            f << line
           end
         end
-    end
-
-    def write_current_validator_balances(epoch)
-        write_json(current_validator_balances(epoch))
     end
 
 end
