e6e9df486b5dbb094ae3fc21b3aba5b192d4088c
Timo <me@timosci.net>
2022-06-10 00:40:38 +0200

    turn default filename into instance variable

--- src/report.rb
+++ src/report.rb
@@ -8,27 +8,28 @@ class Report
     def initialize(clients=Eth2Checklist.new.clients, metrics=ValidatorMetricsInterface.new)
         @clients = clients
         @metrics = metrics
+        @default_filename = "validator-balances"
     end
 
-    attr_accessor :clients, :metrics
+    attr_accessor :clients, :metrics, :default_filename
 
     def current_validator_balances(epoch)
         pubkeys = metrics.validator_balances_base64.map{|h| h[:pubkey64]}
         clients.prysmbeacon.interface.balances_for_epoch(epoch,pubkeys)
     end
 
-    def write_json(data,file="validator-balances.json")
+    def write_json(data,file="#{default_filename}.json")
         File.open(file,"w") do |f|
             f.write(data.to_json)
         end
     end
 
-    def read_json(file)
+    def read_json(file="#{default_filename}.json")
         file = "#{file}.json"
         JSON.parse(File.read(file))
     end
 
-    def write_csv(data,file="validator-balances.csv")
+    def write_csv(data,file="#{default_filename}.csv")
         CSV.open("#{file}.csv","wb") do |f|
           data.each do |name,properties|
             f << [name,properties]
